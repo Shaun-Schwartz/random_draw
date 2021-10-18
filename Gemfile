a89fb198be497dd031bf3802152d3641e10be97d
Shaun Schwartz <me@shaunschwartz.com>
2021-10-18 08:49:23 -0700

    init

---
+++ Gemfile
@@ -0,0 +1,4 @@
+gem 'ruby_svg_image_generator'
+gem 'byebug', '11.1.3'
+gem 'victor'
+gem 'chunky_png'
---
+++ colors.rb
@@ -0,0 +1,17 @@
+require 'ostruct'
+require 'pry-byebug'
+
+module Colors
+  ### Blues
+  SKY = OpenStruct.new(hue: '#4D8FAC', weight: 5)
+  PICTON = OpenStruct.new(hue: '#22A7F0', weight: 3)
+  DODGER = OpenStruct.new(hue: '#19B5FE', weight: 2)
+  GREY_BLUE = OpenStruct.new(hue: '#59ABE3', weight: 3)
+  LIGHT_BLUE = OpenStruct.new(hue: '#48929B', weight: 4)
+  THOUSAND_HERB = OpenStruct.new(hue: '#317589', weight: 5)
+  JORDY = OpenStruct.new(hue: '#89C4F4', weight: 5)
+  STEEL = OpenStruct.new(hue: '#4B77BE', weight: 4)
+  LAPIS_LAZULI= OpenStruct.new(hue: '#1F4788', weight: 4)
+  NAVY_BLUE = OpenStruct.new(hue: '#003171', weight: 1)
+
+end
