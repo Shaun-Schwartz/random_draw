0622c4126597b1e19dcdf526ad3ddc90ed8b2006
Shaun-Schwartz <33434280+Shaun-Schwartz@users.noreply.github.com>
2021-11-05 15:35:34 -0700

    Add new constants

--- constants.rb
+++ constants.rb
@@ -4,4 +4,15 @@ module Constants
   MAX_HEIGHT = 1024
   MAX_SHAPES = 3
 
+  SUPER_COMMON = 'super common'
+  VERY_COMMON = 'very common'
+  COMMON = 'common'
+  RARE = 'rare'
+  SUPER_RARE = 'super rare'
+  SUPER_COMMON_RARITY = 5
+  VERY_COMMON_RARITY = 4
+  COMMON_RARITY = 3
+  RARE_RARITY = 2
+  SUPER_RARE_RARITY = 1
+
 end
