0fe2f1d47182d478831f407539fe5a663507fba3
Shaun-Schwartz <33434280+Shaun-Schwartz@users.noreply.github.com>
2021-11-05 15:34:31 -0700

    Make colors module a class so we can keep track of the colors used for an image

--- colors.rb
+++ colors.rb
@@ -1,108 +1,117 @@
-require 'ostruct'
-require 'pry-byebug'
+require_relative 'constants'
+require_relative 'rarity'
 
-module Colors
+class Colors
+  include Rarity
+  attr_accessor :colors_used
+
+  def initialize
+    @colors_used = []
+  end
 
   YELLOWS = [
-    {bright_golden_yellow: '#FFA400', weight: 1},
-    {ripe_lemon: '#F7CA18', weight: 2},
-    {gamboge: '#FFB61E', weight: 2},
-    {cream_can: '#F5D76E', weight: 3},
-    {corn_colored: '#FAA945', weight: 3},
-    {egg_colored: '#FFA631', weight: 3},
-    {floral_leaf_colored: '#FFB94E', weight: 4},
-    {saffron: '#F4D03F', weight: 4},
-    {sumac_dyed: '#E08A1E', weight: 4},
-    {ecstasy: '#F9690E', weight: 4},
-    {lightning_yellow: '#F5AB35', weight: 5},
-    {japanese_triandra_grass: '#E2B13C', weight: 5},
-    {amur_cork_tree: '#F3C13A', weight: 5},
-    {patrina_flowers: '#D9B611', weight: 5},
-    {golden_fallen_leaves: '#E29C45', weight: 5}
+    {bright_golden_yellow: '#FFA400', rarity: SUPER_RARE_RARITY},
+    {ripe_lemon: '#F7CA18', rarity: RARE_RARITY},
+    {ripe_lemon: '#F7CA18', rarity: RARE_RARITY},
+    {ripe_lemon: '#F7CA18', rarity: RARE_RARITY},
+    {gamboge: '#FFB61E', rarity: RARE_RARITY},
+    {cream_can: '#F5D76E', rarity: COMMON_RARITY},
+    {corn_colored: '#FAA945', rarity: COMMON_RARITY},
+    {egg_colored: '#FFA631', rarity: COMMON_RARITY},
+    {floral_leaf_colored: '#FFB94E', rarity: VERY_COMMON_RARITY},
+    {saffron: '#F4D03F', rarity: VERY_COMMON_RARITY},
+    {sumac_dyed: '#E08A1E', rarity: VERY_COMMON_RARITY},
+    {ecstasy: '#F9690E', rarity: VERY_COMMON_RARITY},
+    {lightning_yellow: '#F5AB35', rarity: SUPER_COMMON_RARITY},
+    {japanese_triandra_grass: '#E2B13C', rarity: SUPER_COMMON_RARITY},
+    {amur_cork_tree: '#F3C13A', rarity: SUPER_COMMON_RARITY},
+    {patrina_flowers: '#D9B611', rarity: SUPER_COMMON_RARITY},
+    {golden_fallen_leaves: '#E29C45', rarity: SUPER_COMMON_RARITY}
   ]
 
   GREENS = [
-    {green_three: '#00CD00', weight: 1},
-    {observatory: '#049372', weight: 2},
-    {turquoise: '#36D7B7', weight: 2},
-    {mountain_medow: '#16A085', weight: 3},
-    {free_speech_aquamarine: '#03A678', weight: 3},
-    {eucalyptus: '#26A65B', weight: 3},
-    {patina: '#407A52', weight: 4},
-    {jungle_green: '#26C281', weight: 4},
-    {jungle_green: '#2ABB9B', weight: 4},
-    {ocean_green: '#4DAF7C', weight: 4},
-    {gossip: '#87D37C', weight: 5},
-    {siskin_sprout_yellow: '#7A942E', weight: 5},
-    {young_green_onion: '#8DB255', weight: 5},
-    {fresh_onion: '#5B8930', weight: 5},
-    {young_bamboo: '#6B9362', weight: 5}
+    {green_three: '#00CD00', rarity: SUPER_RARE_RARITY},
+    {observatory: '#049372', rarity: RARE_RARITY},
+    {turquoise: '#36D7B7', rarity: RARE_RARITY},
+    {mountain_medow: '#16A085', rarity: COMMON_RARITY},
+    {free_speech_aquamarine: '#03A678', rarity: COMMON_RARITY},
+    {eucalyptus: '#26A65B', rarity: COMMON_RARITY},
+    {patina: '#407A52', rarity: VERY_COMMON_RARITY},
+    {jungle_green: '#26C281', rarity: VERY_COMMON_RARITY},
+    {jungle_green: '#2ABB9B', rarity: VERY_COMMON_RARITY},
+    {ocean_green: '#4DAF7C', rarity: VERY_COMMON_RARITY},
+    {gossip: '#87D37C', rarity: SUPER_COMMON_RARITY},
+    {siskin_sprout_yellow: '#7A942E', rarity: SUPER_COMMON_RARITY},
+    {young_green_onion: '#8DB255', rarity: SUPER_COMMON_RARITY},
+    {fresh_onion: '#5B8930', rarity: SUPER_COMMON_RARITY},
+    {young_bamboo: '#6B9362', rarity: SUPER_COMMON_RARITY}
   ]
 
   BLUES = [
-    {aqua: '#00FFFF', weight: 1},
-    {mediumblue: '#0000CD', weight: 2},
-    {darkblue: '#00008B', weight: 2},
-    {royalblue: '#4169E1', weight: 3},
-    {blue: '#0000FF', weight: 3},
-    {midnightblue: '#191970', weight: 3},
-    {cornflowerblue: '#6495ED', weight: 4},
-    {mediumTurquoise: '#48D1CC', weight: 4},
-    {deepskyblue: '#00BFFF', weight: 4},
-    {dodgerblue: '#1E90FF', weight: 4},
-    {cadetblue: '#5F9EA0', weight: 5},
-    {steelblue: '#4682B4', weight: 5},
-    {lightblue: '#ADD8E6', weight: 5},
-    {skyblue: '#87CEEB', weight: 5},
-    {paleTurquoise: '#AFEEEE', weight: 5}
+    {aqua: '#00FFFF', rarity: SUPER_RARE_RARITY},
+    {medium_blue: '#0000CD', rarity: RARE_RARITY},
+    {dark_blue: '#00008B', rarity: RARE_RARITY},
+    {royal_blue: '#4169E1', rarity: COMMON_RARITY},
+    {blue: '#0000FF', rarity: COMMON_RARITY},
+    {midnight_blue: '#191970', rarity: COMMON_RARITY},
+    {cornflower_blue: '#6495ED', rarity: VERY_COMMON_RARITY},
+    {medium_turquoise: '#48D1CC', rarity: VERY_COMMON_RARITY},
+    {deepsky_blue: '#00BFFF', rarity: VERY_COMMON_RARITY},
+    {dodger_blue: '#1E90FF', rarity: VERY_COMMON_RARITY},
+    {cadet_blue: '#5F9EA0', rarity: SUPER_COMMON_RARITY},
+    {steel_blue: '#4682B4', rarity: SUPER_COMMON_RARITY},
+    {light_blue: '#ADD8E6', rarity: SUPER_COMMON_RARITY},
+    {sky_blue: '#87CEEB', rarity: SUPER_COMMON_RARITY},
+    {pale_turquoise: '#AFEEEE', rarity: SUPER_COMMON_RARITY}
   ]
 
   PURPLES = [
-    {fuchsia: '#FF00FF', weight: 1},
-    {purple: '#800080', weight: 2},
-    {indigo: '#4B0082', weight: 2},
-    {mediumorchid: '#BA55D3', weight: 3},
-    {darkviolet: '#9400D3', weight: 3},
-    {blueviolet: '#8A2BE2', weight: 3},
-    {mediumpurple: '#9370DB', weight: 4},
-    {mediumslateblue: '#7B68EE', weight: 4},
-    {slateblue: '#6A5ACD', weight: 4},
-    {rebeccapurple: '#663399', weight: 4},
-    {darkslateblue: '#483D8B', weight: 5},
-    {thistle: '#D8BFD8', weight: 5},
-    {plum: '#DDA0DD', weight: 5},
-    {orchid: '#DA70D6', weight: 5},
-    {violet: '#EE82EE', weight: 5}
+    {fuchsia: '#FF00FF', rarity: SUPER_RARE_RARITY},
+    {purple: '#800080', rarity: RARE_RARITY},
+    {indigo: '#4B0082', rarity: RARE_RARITY},
+    {medium_orchid: '#BA55D3', rarity: COMMON_RARITY},
+    {dark_violet: '#9400D3', rarity: COMMON_RARITY},
+    {blue_violet: '#8A2BE2', rarity: COMMON_RARITY},
+    {medium_purple: '#9370DB', rarity: VERY_COMMON_RARITY},
+    {medium_slate_blue: '#7B68EE', rarity: VERY_COMMON_RARITY},
+    {slate_blue: '#6A5ACD', rarity: VERY_COMMON_RARITY},
+    {rebecca_purple: '#663399', rarity: VERY_COMMON_RARITY},
+    {darkslate_blue: '#483D8B', rarity: SUPER_COMMON_RARITY},
+    {thistle: '#D8BFD8', rarity: SUPER_COMMON_RARITY},
+    {plum: '#DDA0DD', rarity: SUPER_COMMON_RARITY},
+    {orchid: '#DA70D6', rarity: SUPER_COMMON_RARITY},
+    {violet: '#EE82EE', rarity: SUPER_COMMON_RARITY}
   ]
 
   REDS = [
-    {monza: '#CF000F', weight: 1},
-    {radical_red_pink: '#F62459', weight: 2},
-    {pomegranate: '#F22613', weight: 2},
-    {medium_crimson: '#C93756', weight: 3},
-    {foreign_crimson: '#C91F37', weight: 3},
-    {peach_pink: '#F47983', weight: 3},
-    {one_kin_dye: '#F08F90', weight: 4},
-    {pure_crimson: '#C3272B', weight: 4},
-    {true_red: '#8F1D21', weight: 4},
-    {chestnut_rose: '#D24D57', weight: 4},
-    {red_orange: '#DC3023', weight: 5},
-    {red_plum: '#DB5A6B', weight: 5},
-    {cherry_blossom: '#FCC9B9', weight: 5},
-    {washed_out_crimson: '#FFB3A7', weight: 5},
-    {ibis_wing: '#F58F84', weight: 5},
+    {monza: '#CF000F', rarity: SUPER_RARE_RARITY},
+    {radical_red_pink: '#F62459', rarity: RARE_RARITY},
+    {pomegranate: '#F22613', rarity: RARE_RARITY},
+    {medium_crimson: '#C93756', rarity: COMMON_RARITY},
+    {foreign_crimson: '#C91F37', rarity: COMMON_RARITY},
+    {peach_pink: '#F47983', rarity: COMMON_RARITY},
+    {one_kin_dye: '#F08F90', rarity: VERY_COMMON_RARITY},
+    {pure_crimson: '#C3272B', rarity: VERY_COMMON_RARITY},
+    {true_red: '#8F1D21', rarity: VERY_COMMON_RARITY},
+    {chestnut_rose: '#D24D57', rarity: VERY_COMMON_RARITY},
+    {red_orange: '#DC3023', rarity: SUPER_COMMON_RARITY},
+    {red_plum: '#DB5A6B', rarity: SUPER_COMMON_RARITY},
+    {cherry_blossom: '#FCC9B9', rarity: SUPER_COMMON_RARITY},
+    {washed_out_crimson: '#FFB3A7', rarity: SUPER_COMMON_RARITY},
+    {ibis_wing: '#F58F84', rarity: SUPER_COMMON_RARITY},
   ]
 
-  def self.random_color
-    weight = self.weighted_random_num
+  def random_color
+    rarity = rarityed_random_num
     all = YELLOWS + GREENS + BLUES + PURPLES + REDS
-    matched_colors = all.select { |c| c[:weight] == weight }
+    matched_colors = all.select { |c| c[:rarity] == rarity }
     color_hash = matched_colors.sample
+    @colors_used << color_hash
     color_hash[color_hash.keys.first]
   end
 
-  def self.weighted_random_num
-  # 1 = 4, 2 = 12%, 3 = 20%, 4 = 28%, 5 = 36%
+  def rarityed_random_num
+    # 1 = 4, 2 = 12%, 3 = 20%, 4 = 28%, 5 = 36%
     a = rand(1..5)
     b = rand(1..5)
     [a, b].max
